abf053283e356120ecc3493d7eee538e93c1c2d9
Ronald Nidera <nideraronald.work@gmail.com>
2026-01-02 22:33:52 +0800

    added LICENSE to all header files and all source code files.

--- modules/check-and-set.h
+++ modules/check-and-set.h
@@ -1,3 +1,16 @@
+/*******************************************************************************
+ * OpenPAS (Open Portable Atomic-Chemistry Simulator)                          *
+ * Copyright (C) 2026 Ronald Nidera                                            *
+ * *
+ * This program is free software: you can redistribute it and/or modify        *
+ * it under the terms of the GNU General Public License as published by        *
+ * the Free Software Foundation, either version 3 of the License, or           *
+ * (at your option) any later version.                                         *
+ * *
+ * Any redistribution of this code MUST retain this copyright notice and       *
+ * give credit to the original author: Ronald Nidera.                          *
+ *******************************************************************************/
+
 #ifndef CHECKANDSET_H
 #define CHECKANDSET_H
 
